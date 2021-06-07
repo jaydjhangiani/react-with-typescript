82f7dd6de489587c34eb2b1513827b223772b078
Jay Jhangiani <jayjhangiani@Jays-MacBook-Air.local>
2021-06-07 12:14:52 +0530

    completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import AddToList from './components/AddToList';
+import List from './components/List'
+//  ? is optional
+
+export interface IState {
+  people: {
+    name : string
+    age: number
+    url: string
+    note?: string
+  }[]
+}
 
 function App() {
+
+  const [people,setPeople] = useState<IState["people"]>([
+    {
+    name: "Jay",
+    url:"https://storage.jewheart.com/content/users/avatars/3746/avatar_3746_500.jpg?1558628223",
+    age: 21,
+    note:"This is a test for Jay"
+  },
+])
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>People invited to my party</h1>
+      <List people={people} />
+      <AddToList setPeople={setPeople} people={people}/>
     </div>
   );
 }
---
+++ src/components/AddToList.tsx
@@ -0,0 +1,53 @@
+import React, { useState } from 'react'
+import {IState as Props} from '../App'
+
+interface IProps {
+    people: Props["people"]
+    setPeople: React.Dispatch<React.SetStateAction<Props["people"]>>
+}
+
+const AddToList: React.FC<IProps> = ({people, setPeople}) => {
+
+    const [input, setInput] = useState({
+        name:"",
+        age:"",
+        note:"",
+        img:""
+    })
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>): void => {
+
+        setInput({
+            ...input, 
+            [e.target.name]: e.target.value
+        })
+    }
+
+    const handleClick = (): void => {
+        if(!input.name || !input.age || !input.img){
+            return
+        }
+
+        setPeople([
+            ...people,
+            {
+                name: input.name,
+                age: parseInt(input.age),
+                url: input.img,
+                note: input.note
+            }
+        ])
+    }
+
+    return (
+        <div className="AddToList">
+            <input type="text" className="AddToList-input" placeholder="Name" value={input.name} onChange={handleChange} name="name" />
+            <input type="text" className="AddToList-input" placeholder="Age" value={input.age} onChange={handleChange} name="age"/>
+            <input type="text" className="AddToList-input" placeholder="Image Url" value={input.img} onChange={handleChange} name="img"/>
+            <textarea className="AddToList-input" placeholder="Notes" value={input.note} onChange={handleChange} name="note"/>
+            <button className="AddToList-btn" onClick={handleClick}>Add to List</button>
+        </div>
+    )
+}
+
+export default AddToList
